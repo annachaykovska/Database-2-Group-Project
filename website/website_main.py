56e3e9f3df7c101b609da662c1fc067b62149249
annachaykovska <60158249+annachaykovska@users.noreply.github.com>
2021-11-12 18:41:21 -0700

    hook up basic sqlite database

--- website/website_main.py
+++ website/website_main.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Can change this to be another db
+db = SQLAlchemy(app)
+
+
+# This is the structure of the database table to use
+class Courses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    courseNumber = db.Column(db.String(120), nullable=False)
+    courseCode = db.Column(db.String(4), nullable=False)
+    prerequisites = db.Column(db.String(200), nullable=True)
+    antirequisites = db.Column(db.String(200), nullable=True)
+
+
+# TODO: This will just make a new database if there isn't one here already, probably will need
+#  to remove this
+db.create_all()
 
 
 @app.route("/")
